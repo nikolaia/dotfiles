8f1a32529b89776837d0a378ce369bbf31b7b899
nikolaia <nikolai@stasj.com>
2025-03-19 13:57:02 +0100

    chore: nix flake update

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -1,6 +1,5 @@
 { self, nixpkgs, ... }:
 {
-  services.nix-daemon.enable = true;
   nix.settings.experimental-features = "nix-command flakes";
   programs.fish.enable = true;
   system.configurationRevision = self.rev or self.dirtyRev or null;
