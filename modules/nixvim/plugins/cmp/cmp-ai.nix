a989a5162afdb1638add4607d133a30c79e953f0
nikolaia <nikolai@stasj.com>
2025-05-23 10:10:03 +0200

    chore: disable ollama completions

--- modules/nixvim/plugins/cmp/cmp-ai.nix
+++ modules/nixvim/plugins/cmp/cmp-ai.nix
@@ -15,27 +15,27 @@
     };
   };
 
-  plugins.ollama = {
-    enable = true;
-    model = "codellama:13b-code";
-    action = "display";
-  };
-
-  plugins.cmp-ai = {
-    enable = true;
-    settings = {
-      max_lines = 100;
-      provider = "Ollama";
-      provider_options = {
-        model = "codellama:13b-code";
-      };
-      notify = true;
-      notify_callback = "function(msg) vim.notify(msg) end";
-      run_on_every_keystroke = true;
-      ignored_file_types = {
-        # Empty by default, example of how to ignore:
-        # lua = true;
-      };
-    };
-  };
+  # plugins.ollama = {
+  #   enable = true;
+  #   model = "codellama:13b-code";
+  #   action = "display";
+  # };
+  #
+  # plugins.cmp-ai = {
+  #   enable = true;
+  #   settings = {
+  #     max_lines = 100;
+  #     provider = "Ollama";
+  #     provider_options = {
+  #       model = "codellama:13b-code";
+  #     };
+  #     notify = true;
+  #     notify_callback = "function(msg) vim.notify(msg) end";
+  #     run_on_every_keystroke = true;
+  #     ignored_file_types = {
+  #       # Empty by default, example of how to ignore:
+  #       # lua = true;
+  #     };
+  #   };
+  # };
 }
